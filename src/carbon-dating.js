b557af125187805f62a45f2a0dbd068cc634d70d
anticher <anticherq@gmail.com>
2021-09-06 18:02:54 +0300

    feat: complete carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,7 +2,7 @@ import { NotImplementedError } from '../extensions/index.js';
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
-
+const k = 0.693
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
@@ -17,7 +17,30 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+export default function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  // if (typeof sampleActivity !== 'string') {
+  //   return false
+  // } else if (isNaN(parseInt(sampleActivity))) {
+  //   return false
+  // } else if (sampleActivity === '') {
+  //   return false
+  // } else if (sampleActivity === undefined) {
+  //   return false
+  // } else if (sampleActivity === null) {
+  //   return false
+  // } else if (sampleActivity < 1) {
+  //   return false
+  // } else if (sampleActivity > 15) {
+  //   return false
+  // } 
+  if (typeof sampleActivity !== 'string' || +sampleActivity < 1 || +sampleActivity > 15 || isNaN(+sampleActivity)) {
+    return false
+  } else {
+    
+    let answer = Math.log(MODERN_ACTIVITY / +sampleActivity) / (k / HALF_LIFE_PERIOD)
+
+    return Math.ceil(answer)
+  }
 }

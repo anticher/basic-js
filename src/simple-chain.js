f9ccdff5f1732ddcde6cacb05e6e61e1e06a4396
anticher <anticherq@gmail.com>
2021-09-09 21:15:38 +0300

    feat: complete simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,25 +4,42 @@ import { NotImplementedError } from '../extensions/index.js';
  * Implement chainMaker object according to task description
  * 
  */
-export default {
+let chainMaker;
+export default chainMaker = {
+  arr: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.arr.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.arr.push('( ' + value + ' )')
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (this.arr[position - 1] === undefined) {
+      this.arr = []
+      throw Error("You can't remove incorrect link!")
+    } else {
+      this.arr.splice(position - 1, 1)
+      return this
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.arr.reverse()
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    let result = this.arr.join('~~')
+    this.arr = []
+    return result
   }
 };

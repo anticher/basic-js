5c7376405deae31fc7200c31c0cb36663370f877
anticher <anticherq@gmail.com>
2021-09-11 09:59:04 +0300

    feat: complete extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,44 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+export default function repeater(str, { repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|' }) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+//   let defaults = {
+//     'repeatTimes'  :   '',
+//     'separator'  :   '+',
+//     'addition'  :   '',
+//     'additionRepeatTimes'  :   '',
+//     'additionSeparator'  :   '|',
+// }
+
+// for(let i in defaults) 
+//     if(typeof options[i] == "undefined") 
+//            options[i] = defaults[i];
+let arr =[]
+  function check(str) {
+    if (typeof str !== 'string') {
+      return`${str}`
+    }
+    return str
+  }
+    str = check(str)
+    addition = check(addition)
+  
+  for (let i = 0; i < repeatTimes; i++) {
+    arr.push(str)
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      arr.push(addition)
+      arr.push(additionSeparator)
+      if (j === additionRepeatTimes - 1) {
+        arr.pop()
+        arr.push(separator)
+      }
+    }
+    if (i === repeatTimes - 1) {
+      arr.pop()
+    }
+  }
+  return arr.join('')
 }

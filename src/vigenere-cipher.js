8da88efd2cbc2477a63a1d868b99f75da50cdcdd
anticher <anticherq@gmail.com>
2021-09-19 23:59:26 +0300

    feat: complete vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,76 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(modification) {
+    if (modification === false) {
+      this.modification = 'reverse'
+    } else {
+      this.modification = 'straight'
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+  encrypt(word, key) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (word === undefined || key === undefined) {
+      throw Error('Incorrect arguments!')
+    }
+    key = key.repeat(Math.ceil(word.length / key.length))
+    let start = 'A'.charCodeAt(0)
+    let alphaLength = 26
+    key = key.toUpperCase()
+    word = word.toUpperCase()
+    let wordArr = word.split('')
+    let resultArr = []
+    let spaces = 0
+    let re = new RegExp('[A-Z]')
+    wordArr.forEach((element, index) => {
+      if (element === ' ' || !re.test(element)) {
+        resultArr.push(element)
+        spaces++
+      } else {
+        let letterIndex = element.charCodeAt(0) - start
+        let shift = key[index - spaces].charCodeAt(0) - start
+        console.log(key[index])
+        console.log(key)
+        resultArr.push(String.fromCharCode(start + (letterIndex + shift + alphaLength) % alphaLength))
+      }
+    })
+    if (this.modification === 'reverse') {
+      resultArr.reverse()
+    }
+    return resultArr.join('')
+  }
+  decrypt(word, key) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    if (word === undefined || key === undefined) {
+      throw Error('Incorrect arguments!')
+    }
+    if (key.length < word.length) {
+      key = key.repeat(Math.ceil(word.length / key.length))
+    }
+    let start = 'A'.charCodeAt(0)
+    let alphaLength = 26
+    key = key.toUpperCase()
+    word = word.toUpperCase()
+    let wordArr = word.split('')
+    let resultArr = []
+    let spaces = 0
+    let re = new RegExp('[A-Z]')
+    wordArr.forEach((element, index) => {
+      if (element === ' ' || !re.test(element))  {
+        resultArr.push(element)
+        spaces++
+      } else {
+        let letterIndex = element.charCodeAt(0) - start
+        let shift = key[index - spaces].charCodeAt(0) - start
+        resultArr.push(String.fromCharCode(start + (letterIndex - shift + alphaLength) % alphaLength))
+      }
+    })
+    if (this.modification === 'reverse') {
+      resultArr.reverse()
+    }
+    return resultArr.join('')
   }
 }
+
